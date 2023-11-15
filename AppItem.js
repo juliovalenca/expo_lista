a91473e85bb4be881338b9a7c0147b5ebe60b421
juliovalenca <141372869+juliovalenca@users.noreply.github.com>
2023-11-14 21:03:11 -0300

    Update AppItem.js
    adicionando o código de hoje

--- AppItem.js
+++ AppItem.js
@@ -1,15 +1,72 @@
 import React from 'react';
-import {Stelysheet, Text,View,TouchableOpacity} from 'react-nateve';
+import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import Database from './Database'
 
 export default function AppItem(props){
+    async function handleEditPress(){
+        const item = await Database.getItem(props.id);
+        props.navigation.navigate("AppForms", item);
+    }
     return(
-        <View style={style.container}>
-            <Text style={style.textItem}>{props.item}</Text>
-            <View style={style.buttonContainer}>
-                <TouchableOpacity style={style.deletButton}>
-                    <Text style={style.buttonText}>Editar</Text>
+        <View style={styles.container}>
+            <Text style={styles.textItem}>{props.item}</Text>
+            <View style={styles.buttonsContainer}>
+                <TouchableOpacity style={styles.deleteButton} >
+                    <Text style={styles.buttonText}>X</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.editButton}
+                onPress={handleEditPress} >
+                    <Text style={styles.buttonText}>Editar</Text>
                 </TouchableOpacity>
             </View>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+      backgroundColor: '#fff',
+      marginTop: 20,
+      width: '100%'
+    },
+    buttonsContainer: {
+        flexDirection: 'row-reverse',
+        alignItems: 'flex-end',
+        borderBottomWidth: 1,
+        borderBottomColor: '#CCC',
+        paddingBottom: 10,
+        marginTop: 10,
+    },
+    editButton: {
+        marginLeft: 10,
+        height: 40,
+        backgroundColor: 'blue',
+        borderRadius: 10,
+        padding: 10,
+        fontSize: 12,
+        elevation: 10,
+        shadowOpacity: 10,
+        shadowColor: '#ccc',
+        alignItems: 'center'
+    },
+    deleteButton: {
+        marginLeft: 10,
+        height: 40,
+        width: 40,
+        backgroundColor: 'red',
+        borderRadius: 10,
+        padding: 10,
+        fontSize: 12,
+        elevation: 10,
+        shadowOpacity: 10,
+        shadowColor: '#ccc',
+        alignItems: 'center'
+    },
+    buttonText: {
+        color: '#fff',
+        fontWeight: 'bold',
+    },
+    textItem: {
+        fontSize: 20,
+    }
+  });
